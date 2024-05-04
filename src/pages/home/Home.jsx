a58189b0f624d56ab134910f55465912fd7726ba
Resiandra Bima <111202113665@mhs.dinus.ac.id>
2024-05-04 13:49:23 +0700

    ubah teks di app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
 import "./home.scss";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar/>
+          <div className="widget">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+         <Widget type="balance"/>
+      </div>
+      <div className="charts">
+        <Chart type="order" aspect={2 / 1} />
+        <Chart type="earning" aspect={2 / 1} />
     </div>
-  )
-}
+  </div>
+  </div>
+  );
+};
 
-export default Home
+export default Home;
